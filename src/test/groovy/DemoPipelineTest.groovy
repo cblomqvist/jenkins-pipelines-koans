b5d1e91aff6284ef275043ea129d7cc73c31ce46
Blomqvist Christer <christer.blomqvist@volvo.com>
2019-10-30 18:23:05 +0100

    follow along in live coding demo

--- src/test/groovy/DemoPipelineTest.groovy
+++ src/test/groovy/DemoPipelineTest.groovy
@@ -9,4 +9,35 @@ class DemoPipelineTest extends Specification {
         runner = new PipelineTestRunner()
         runner.preferClassLoading = false
     }
+
+    def 'check if pipeline is loaded correctly'() {
+        given:
+        def stepScript = runner.load {
+            script 'vars/demoPipeline.groovy'
+        }
+
+        when:
+        stepScript()
+
+        then:
+        assert stepScript.class.canonicalName == 'demoPipeline'
+    }
+
+    def 'check if sh command is called'() {
+        given:
+        List testSh = []
+        def stepScript = runner.load {
+            script 'vars/demoPipeline.groovy'
+            method ('sh', [String]) {
+                str -> testSh.add(str)
+            }
+        }
+
+        when:
+        stepScript()
+
+        then:
+        assert testSh.contains('gradle build')
+        assert testSh.size() == 1
+    }
 }
